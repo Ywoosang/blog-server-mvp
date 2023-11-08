f5ef9b01c660179c8284f7a1db57006f2e975e41
ywoosang <opellong13@gmail.com>
2023-11-08 11:44:06 +0000

    feat(like): like 엔티티 추가

---
+++ src/like/entities/like.entity.ts
@@ -0,0 +1,26 @@
+import { 
+    Entity, 
+    PrimaryGeneratedColumn,
+    ManyToOne,
+    JoinColumn,
+    CreateDateColumn
+} from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Post } from 'src/post/entities/post.entity';
+
+@Entity()
+export class Like {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @CreateDateColumn()  
+    createdAt: Date;
+
+    @ManyToOne(()=> User, user => user.likes)
+    @JoinColumn({ name: 'userId' })
+    user: User;
+
+    @ManyToOne(()=> Post, post => post.likes)
+    @JoinColumn({ name: 'postId' })
+    post: Post;
+}
