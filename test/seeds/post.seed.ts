7c3e88ce39f0e4154b3b23160d4898fe42061166
ywoosang <opellong13@gmail.com>
2023-12-07 14:34:29 +0000

    feat(test): 테스트 seed 에 테스트 데이터를 위한 faker 추가

--- test/seeds/post.seed.ts
+++ test/seeds/post.seed.ts
@@ -2,36 +2,32 @@ import { PostStatus } from 'src/post/post-status.enum';
 import { PostService } from 'src/post/post.service';
 import { User } from 'src/users/entities/user.entity';
 import { CreatePostDto } from 'src/post/dto/create-post.dto';
+import * as faker from 'faker';
 
 class PostSeeder {
     constructor(private postService: PostService) {}
 
-    async createTestPosts(
-        user: User,
-		count: number,
-        status: PostStatus
-	) {        
-        const userPromises = Array.from({ length: count }, async (_, i) => {
-            const post: CreatePostDto   = {
-                title: `테스트 제목${i}`,
-                content: `테스트 내용${i}`,
+    async createTestPosts(count: number, user: User, status: PostStatus) {
+        const userPromises = Array.from({ length: count }, async () => {
+            const post: CreatePostDto = {
+                title: faker.lorem.sentence(),
+                content: faker.lorem.paragraph(),
                 status
-            }
-            return this.postService.create(post,user);
+            };
+
+            return this.postService.create(post, user);
         });
+
         return Promise.all(userPromises);
     }
 
-    async createTestPost(
-        user: User,
-        status: PostStatus
-    ) {
+    async createTestPost(user: User, status: PostStatus) {
         const post: CreatePostDto = {
-            title: `테스트 제목`,
-            content: '테스트 내용',
+            title: faker.lorem.sentence(),
+            content: faker.lorem.paragraph(),
             status
-        }
-        await this.postService.create(post,user);
+        };
+        await this.postService.create(post, user);
     }
 }
 
