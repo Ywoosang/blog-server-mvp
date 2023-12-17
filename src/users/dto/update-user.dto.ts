ec82b84c04e7ceff50d683b66fce0bf982d4d8d9
ywoosang <opellong13@gmail.com>
2023-12-17 07:09:46 +0000

    fix(user): 닉네임 필수 변경 해제

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,11 +1,12 @@
 import { IsString, IsOptional, MaxLength, MinLength } from 'class-validator';
 
 export class UpdateUserDto {
+    @IsOptional()
     @IsString()
     @MinLength(2)
     @MaxLength(10)
-    nickname: string;
+    nickname?: string;
 
     @IsOptional()
-    description: string;
+    description?: string;
 }
