d6aef5010bdd757b731cd8ef5b0373ebfb4799a9
ywoosang <opellong13@gmail.com>
2024-01-06 01:55:53 +0000

    fix(post): 게시물 모듈에 카테고리, 태그 모듈 추가 fixed #4

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -5,9 +5,11 @@ import { PostService } from './post.service';
 import { Post } from './entities/post.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { UsersModule } from 'src/users/users.module';
+import { TagModule } from 'src/tag/tag.module';
+import { CategoryModule } from 'src/category/category.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Post]), AuthModule, UsersModule],
+    imports: [TypeOrmModule.forFeature([Post]), AuthModule, UsersModule, TagModule, CategoryModule],
     providers: [PostService],
     exports: [PostService],
     controllers: [PostController]
