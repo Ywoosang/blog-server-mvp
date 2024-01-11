0e9ea0f42cd8a407eea074cc858d34b9a8293e28
ywoosang <opellong13@gmail.com>
2024-01-11 13:31:37 +0000

    feat(cors): 클라이언트에서의 다른 도메인에서의 요청을 허용하기 위해 CORS 활성화

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AllConfigType } from 'src/configs/types/config.type';
 import validationOptions from './utils/validation-options';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService<AllConfigType>);
     const port = process.env.APP_PORT;
-
+    app.enableCors({
+        origin: configService.get('app.frontendDomain', { infer: true }),
+        credentials: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        allowedHeaders: 'Content-Type, Accept'
+    });
     app.useGlobalPipes(new ValidationPipe(validationOptions));
+
     await app.listen(port);
     Logger.log(`Application running on port ${port}`);
 }
