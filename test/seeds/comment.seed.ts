7759d391ba6bbf8cb053ed51e9018d41e6d8899e
ywoosang <opellong13@gmail.com>
2024-01-29 11:01:19 +0000

    chore(test): 댓글 seeder 에 replyToId 추가

--- test/seeds/comment.seed.ts
+++ test/seeds/comment.seed.ts
@@ -3,23 +3,27 @@ import { CommentService } from 'src/comment/comment.service';
 import { User } from 'src/users/entities/user.entity';
 import { CreateCommentDto } from 'src/comment/dto/create-comment.dto';
 import { CreateReplyDto } from 'src/comment/dto/create-reply.dto';
+import { Comment } from 'src/comment/entities/comment.entity';
 
 class CommentSeeder {
     constructor(private readonly commentService: CommentService) {}
 
-    async createTestComment(user: User) {
+    async createTestComment(user: User, postId: number): Promise<Comment> {
         const comment: CreateCommentDto = {
             content: faker.lorem.paragraph(),
-            postId: 1
+            postId
         };
-        await this.commentService.create(comment, user);
+
+        return this.commentService.create(comment, user);
     }
 
-    async createTestReply(user: User, parentCommentId: number) {
+    async createTestReply(user: User, parentCommentId: number): Promise<Comment> {
         const reply: CreateReplyDto = {
-            content: faker.lorem.paragraph()
+            content: faker.lorem.paragraph(),
+            replyToId: user.id
         };
-        await this.commentService.createReply(reply, parentCommentId, user);
+
+        return this.commentService.createReply(reply, parentCommentId, user);
     }
 }
 
