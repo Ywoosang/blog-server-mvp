aa554f9c4f04fb5b8589b30e84013992827b0d3e
ywoosang <opellong13@gmail.com>
2024-01-29 11:02:04 +0000

    chore(test): 게시물 seeder 가 void 대신 Promise<Post> 를 반환하도록 변경

--- test/seeds/post.seed.ts
+++ test/seeds/post.seed.ts
@@ -3,12 +3,13 @@ import { PostStatus } from 'src/post/post-status.enum';
 import { PostService } from 'src/post/post.service';
 import { User } from 'src/users/entities/user.entity';
 import { CreatePostDto } from 'src/post/dto/create-post.dto';
+import { Post } from 'src/post/entities/post.entity';
 
 class PostSeeder {
     constructor(private readonly postService: PostService) {}
 
-    async createTestPost(user: User, status: PostStatus = PostStatus.PUBLIC, categoryId?: number, tagNames?: string[]) {
-        const post: CreatePostDto = {
+    async createTestPost(user: User, status: PostStatus = PostStatus.PUBLIC, categoryId?: number, tagNames?: string[]): Promise<Post> {
+        const createPostDto: CreatePostDto = {
             title: faker.lorem.sentence(),
             content: faker.lorem.paragraph(),
             description: '',
@@ -17,7 +18,8 @@ class PostSeeder {
             tagNames
         };
 
-        await this.postService.create(post, user);
+        const post = await this.postService.create(createPostDto, user);
+        return post;
     }
 }
 
