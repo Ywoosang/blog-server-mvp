b73395d8670c9912d13ff5738e5b7376bcc40bca
ywoosang <opellong13@gmail.com>
2024-02-07 11:05:38 +0000

    feat(file): 사용자 프로필 이미지 업로드 구현

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -7,27 +7,58 @@ import path from 'path';
 @Injectable()
 export class FilesService {
     private readonly baseDir: string;
+    private readonly tempPath: string;
+    private readonly staticPath: string;
+    private readonly backendDomain: string;
 
     constructor(private readonly configService: ConfigService<AllConfigType>) {
         this.baseDir = this.configService.get('app.workingDirectory', { infer: true });
+        this.tempPath = path.join(this.baseDir, 'public', 'temp');
+        this.backendDomain = this.configService.get('app.backendDomain', { infer: true });
+        this.staticPath = path.join(
+            'static',
+            'images',
+        );
     }
 
     async uploadPostImage(filename: string, postId: string): Promise<string> {
-        const tempPath = path.join(this.baseDir, 'public', 'temp');
-
         const destination = path.join(this.baseDir, 'public', 'images', 'posts', postId);
 
         if (!fs.existsSync(destination)) {
             fs.mkdirSync(destination, { recursive: true });
         }
         // basename
-        const tempFilePath = path.join(tempPath, filename);
+        const tempFilePath = path.join(this.tempPath, filename);
         const filePath = path.join(destination, filename);
 
         // 파일을 지정된 경로로 이동
         await fs.promises.rename(tempFilePath, filePath);
 
-        return filePath;
+        const extention = path.join(
+            this.staticPath,
+            'posts',
+            postId,
+            filename
+        );
+        return `${this.backendDomain}/${extention}`;
+    }
+
+    async uploadUserProfileImage(filename: string) {
+        console.log(console.log(this.configService.get('app.backendDomain', { infer: true })))
+        console.log(filename);
+        const destination = path.join(this.baseDir, 'public', 'images', 'users');
+        if (!fs.existsSync(destination)) {
+            fs.mkdirSync(destination, { recursive: true });
+        }
+        const tempFilePath = path.join(this.tempPath, filename);
+        const filePath = path.join(destination, filename);
+        await fs.promises.rename(tempFilePath, filePath);
+        const extention = path.join(
+            this.staticPath,
+            'users',
+            filename
+        );
+        return `${this.backendDomain}/${extention}`;
     }
 
     deletePostImages(postId: string): void {
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,10 @@ import { UsersController } from './users.controller';
 import { GravatarModule } from 'src/gravatar/gravatar.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { FilesModule } from 'src/files/files.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User]), GravatarModule],
+    imports: [TypeOrmModule.forFeature([User]), GravatarModule, FilesModule],
     controllers: [UsersController],
     providers: [UsersService],
     exports: [UsersService]
