8bfaf25327df1acb390e63c9183edb1752b89ff0
ywoosang <opellong13@gmail.com>
2024-02-09 10:37:34 +0000

    refactor(auth): 회원가입 dto 를 정규표현식으로 통일

--- src/auth/dto/auth-register.dto.ts
+++ src/auth/dto/auth-register.dto.ts
@@ -1,25 +1,22 @@
-import { IsAlphanumeric, IsNotEmpty, IsEmail, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class AuthRegisterDto {
     @IsNotEmpty()
-    @IsEmail()
+    @Matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
     email: string;
 
-    @IsAlphanumeric()
-    @MinLength(4)
-    @MaxLength(20)
+    // 4~15자 영문 또는 숫자
+    @IsString()
+    @Matches(/^[a-zA-Z0-9]{4,10}$/)
     userLoginId: string;
 
+    // 2~10자 영문 또는 한글 또는 숫자
     @IsString()
-    @MinLength(2)
-    @MaxLength(10)
+    @Matches(/^[a-zA-Z가-힣0-9]{2,10}$/)
     nickname: string;
 
+    // 6~15자 영문, 숫자, 특수문자(@!) 각각 적어도 하나씩 포함
     @IsString()
-    @MinLength(6)
-    @MaxLength(15)
-    @Matches(/^[a-zA-Z0-9!@]*$/, {
-        message: 'password only accepts english and number'
-    })
+    @Matches(/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@!])[a-zA-Z0-9@!]{6,15}$/)
     password: string;
 }
