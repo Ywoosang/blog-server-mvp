3368a7f792e48648bc1410d7c03cb1bb120d9a97
ywoosang <opellong13@gmail.com>
2024-02-12 03:18:19 +0000

    refactor(auth): 회원가입 dto 변경

--- src/auth/dto/auth-register.dto.ts
+++ src/auth/dto/auth-register.dto.ts
@@ -1,22 +1,20 @@
-import { IsNotEmpty, IsString, Matches } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class AuthRegisterDto {
     @IsNotEmpty()
-    @Matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+    @IsEmail()
     email: string;
 
     // 4~15자 영문 또는 숫자
     @IsString()
     @Matches(/^[a-zA-Z0-9]{4,10}$/)
-    userLoginId: string;
+    userId: string;
 
     // 2~10자 영문 또는 한글 또는 숫자
     @IsString()
     @Matches(/^[a-zA-Z가-힣0-9]{2,10}$/)
     nickname: string;
 
-    // 6~15자 영문, 숫자, 특수문자(@!) 각각 적어도 하나씩 포함
     @IsString()
-    @Matches(/^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@!])[a-zA-Z0-9@!]{6,15}$/)
-    password: string;
+    description: string;
 }
