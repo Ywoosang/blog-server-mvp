ac13defbf287d14f6acc85999c28389c8bb30109
ywoosang <opellong13@gmail.com>
2024-02-12 03:22:36 +0000

    feat(mail): 회원가입, 로그인용 이메일 전송 로직 추가

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -4,35 +4,65 @@ import path from 'path';
 import { AllConfigType } from 'src/configs/types/config.type';
 import { MailerService } from 'src/mailer/mailer.service';
 import { MailData } from './interfaces/mail-data.interface';
-import { EMAIL_SUBJECT, CONTEXT } from 'src/configs/constants/constants';
+import { LOGIN_SUBJECT, REGISTER_SUBJECT } from 'src/mail/constants/constants';
 
 @Injectable()
 export class MailService {
+    private mailTemplateDir;
+    private baseUrl;
+
     constructor(
         private readonly configService: ConfigService<AllConfigType>,
         private readonly mailerService: MailerService
-    ) {}
+    ) {
+        this.mailTemplateDir = path.join(
+            this.configService.getOrThrow('app.workingDirectory', { infer: true }),
+            'src',
+            'mail',
+            'mail-templates',
+        )
+        this.baseUrl = this.configService.get('app.frontendDomain', { infer: true })
+    }
 
-    async sendVerificationEmail(mailData: MailData<{ hash: string }>): Promise<void> {
-        const url = new URL(`${this.configService.get('app.backendDomain', { infer: true })}/auth/confirm-email`);
+    async sendLoginEmail(mailData: MailData<{ hash: string }>): Promise<void> {
+        const url = new URL(`${this.baseUrl}/auth/login`);
         url.searchParams.set('hash', mailData.data.hash);
 
         await this.mailerService.sendMail({
             to: mailData.to,
-            subject: EMAIL_SUBJECT,
+            subject: LOGIN_SUBJECT,
             templatePath: path.join(
-                this.configService.getOrThrow('app.workingDirectory', { infer: true }),
-                'src',
-                'mail',
-                'mail-templates',
-                'confirmation.hbs'
+                this.mailTemplateDir,
+                'authentication.hbs'
             ),
             context: {
-                title: CONTEXT.TITLE,
-                url: url.toString(),
-                actionTitle: CONTEXT.ACTION_TITLE,
-                app_name: CONTEXT.APP_NAME,
-                text: CONTEXT.TEXT
+                homeUrl: this.baseUrl,
+                actionUrl: url.toString(),
+                actionText: '로그인',
+                greeting: '어서오세요.',
+                linkValidity: 24
+            }
+        });
+    }
+
+
+    async sendRegisterEmail(mailData: MailData<{ hash: string }>): Promise<void> {
+        const url = new URL(`${this.baseUrl}/auth/register`);
+        url.searchParams.set('hash', mailData.data.hash);
+
+        await this.mailerService.sendMail({
+            to: mailData.to,
+            subject: REGISTER_SUBJECT,
+            templatePath: path.join(
+                this.mailTemplateDir,
+                'authentication.hbs'
+            ),
+            context: {
+                homeUrl: this.baseUrl,
+                actionUrl: url.toString(),
+                actionText: '회원가입',
+                greeting: '환영합니다.',
+                linkValidity: 24
             }
         });
     }
