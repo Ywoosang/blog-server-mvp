d4fde35cca77c6014ec21afa70b1ef3372a8dd49
ywoosang <opellong13@gmail.com>
2024-03-15 11:24:23 +0000

    chore(deploy): 스크립트에서 nvm 을 로드할 수 있도록 설정

--- startup.sh
+++ startup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
 REPOSITORY=/home/ec2-user/app
+NVM_DIR="$HOME/.nvm"
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 cd $REPOSITORY
-
-npm ci --production
+ 
+npm ci --omit=dev
 
 pm2 delete all
-
 npm run start:prod
